639dcacc9a1bc7499f8c7a0696731052b0a472c8
Carlos Costa <jr.carlosad@gmail.com>
2021-05-05 21:50:00 -0300

    feat: Controle de mensagens

--- src/views/MessagePanel/components/MessageAssembly/MessageAssembly.tsx
+++ src/views/MessagePanel/components/MessageAssembly/MessageAssembly.tsx
@@ -1,9 +1,50 @@
-import * as S from './MessageAssembly.styles';
+import { useState } from 'react';
+import { Mentions } from 'antd';
 
-const MessageAssembly = (): JSX.Element => (
-  <S.Wrapper>
-    <h2>MessageAssembly</h2>
-  </S.Wrapper>
-);
+import * as S from './MessageAssembly.styles';
+import { useSheet } from '@hooks/Sheet/use-sheet';
+import { replaceMe } from '@utils/strings/index';
+
+const { Option } = Mentions;
+
+const MessageAssembly = (): JSX.Element => {
+  const [message, setMessage] = useState('');
+  const { sheet } = useSheet();
+
+  const getBeautyMessage = (): string => {
+    if (!sheet) {
+      return '';
+    }
+
+    const exampleObject = sheet.refinedData[0];
+
+    return replaceMe(
+      message,
+      (exampleObject as unknown) as Record<string, string>
+    );
+  };
+
+  return (
+    <S.Wrapper>
+      <h2>MessageAssembly</h2>
+
+      <section>
+        {sheet?.headers.map(header => (
+          <span key={header}>{header}</span>
+        ))}
+      </section>
+
+      <Mentions rows={3} onChange={setMessage} prefix={['{']}>
+        {sheet?.headers.map(item => (
+          <Option key={`suggestion-${item}`} value={`${item}}`}>
+            {item}
+          </Option>
+        ))}
+      </Mentions>
+
+      {getBeautyMessage()}
+    </S.Wrapper>
+  );
+};
 
 export default MessageAssembly;
